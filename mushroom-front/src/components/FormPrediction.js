6bcf8fac731dd9bcdbb51726f8e28342f543f39d
corentin5002 <csabier@orange.fr>
2024-06-06 18:59:37 +0200

    Styles - response button

--- mushroom-front/src/components/FormPrediction.js
+++ mushroom-front/src/components/FormPrediction.js
@@ -1,6 +1,6 @@
 'use client'
 import styles from './FormPrediction.module.sass'
-import { getMushrooms} from "@/utils/fetchData"
+import {getMushrooms} from "@/utils/fetchData"
 import {useState} from "react";
 
 export default function FormPrediction() {
@@ -13,44 +13,59 @@ export default function FormPrediction() {
     const [stemColor, setStemColor] = useState('')
     const [season, setSeason] = useState('')
 
+    const [result, setResult] = useState(null) // [0, 1]
     const handleSubmit = async (event) => {
         event.preventDefault();
         const mushroomData = {
-            'cap-diameter': capDiameter,
-            'cap-shape': capShape,
-            'gill-attachment': gillAttachment,
-            'gill-color': gillColor,
-            'stem-height': stemHeight,
-            'stem-width': stemWidth,
-            'stem-color': stemColor,
-            'season': season
+            // transform string to number
+            'cap-diameter': parseFloat(capDiameter),
+            'cap-shape': parseFloat(capShape),
+            'gill-attachment': parseFloat(gillAttachment),
+            'gill-color': parseFloat(gillColor),
+            'stem-height': parseFloat(stemHeight),
+            'stem-width': parseFloat(stemWidth),
+            'stem-color': parseFloat(stemColor),
+            'season': parseFloat(season)
         };
-        console.log('[debug] 1', mushroomData )
         const mushrooms = await getMushrooms(mushroomData);
+        setResult(mushrooms)
+
     };
 
     return (
-        <form onSubmit={handleSubmit} className={styles.form__mushroom} id="mushroom__form__ia__hexagone">
-            <Input label="Cap Shape" type="text" value={capShape} onChange={e => setCapShape(e.target.value)}/>
-            <Input label="CapDiameter" type="text" value={capDiameter} onChange={e => setCapDiameter(e.target.value)}/>
-            <Input label="Gill Attachment" type="text" value={gillAttachment} onChange={e => setGillAttachment(e.target.value)}/>
-            <Input label="Gill Color" type="text" value={gillColor} onChange={e => setGillColor(e.target.value)}/>
-            <Input label="Stem Height" type="text" value={stemHeight} onChange={e => setStemHeight(e.target.value)}/>
-            <Input label="Stem Width" type="text" value={stemWidth} onChange={e => setStemWidth(e.target.value)}/>
-            <Input label="Stem Color" type="text" value={stemColor} onChange={e => setStemColor(e.target.value)}/>
-            <Input label="Season" type="text" value={season} onChange={e => setSeason(e.target.value)}/>
-            <div className={styles.form__mushroom__submit__container}>
-                <button type="submit" className={styles.form__mushroom__submit}>Predict</button>
-            </div>
-        </form>
+        <>
+            <form onSubmit={handleSubmit} className={styles.form__mushroom} id="mushroom__form__ia__hexagone">
+                <Input label="Cap Shape" type="text" value={capShape} onChange={e => setCapShape(e.target.value)}/>
+                <Input label="CapDiameter" type="text" value={capDiameter}
+                       onChange={e => setCapDiameter(e.target.value)}/>
+                <Input label="Gill Attachment" type="text" value={gillAttachment}
+                       onChange={e => setGillAttachment(e.target.value)}/>
+                <Input label="Gill Color" type="text" value={gillColor} onChange={e => setGillColor(e.target.value)}/>
+                <Input label="Stem Height" type="text" value={stemHeight}
+                       onChange={e => setStemHeight(e.target.value)}/>
+                <Input label="Stem Width" type="text" value={stemWidth} onChange={e => setStemWidth(e.target.value)}/>
+                <Input label="Stem Color" type="text" value={stemColor} onChange={e => setStemColor(e.target.value)}/>
+                <Input label="Season" type="text" value={season} onChange={e => setSeason(e.target.value)}/>
+                <div className={styles.form__mushroom__submit__container}>
+                    <button type="submit" className={styles.form__mushroom__submit}>Predict</button>
+                </div>
+            </form>
+            {result !== null && (
+                <div className={styles.form__mushroom__result}>
+                    <p className={styles.form__mushroom__result__text}>
+                        This mushroom is : <span>{result?.prediction}</span>
+                    </p>
+                </div>
+            )}
+        </>
     );
 }
 
-function Input ({label = 'labelUnknown', ...props}){
+function Input({label = 'labelUnknown', ...props}) {
     return (
         <div className={styles.form__mushroom__field__container}>
             <span htmlFor="capShape" className={styles.form__mushroom__field__label}>{label} :</span>
-            <input  placeholder={label} className={styles.form__mushroom__field} {...props}/>
+            <input placeholder={label} className={styles.form__mushroom__field} {...props}/>
         </div>
     )
 }
